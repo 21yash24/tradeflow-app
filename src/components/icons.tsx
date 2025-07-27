9a61e073453dd64f2740f18a0fd971794962bc5b
Yash Jha <yjha974@gmail.com>
2025-07-27 13:55:24 +0000

    THIS FOR APP RIGHT?

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -11,8 +11,9 @@ export const TradeFlowLogo = (props: SVGProps<SVGSVGElement>) => (
     strokeLinejoin="round"
     {...props}
   >
-    <path d="M4 12h16" />
-    <path d="M8 8l-4 4 4 4" />
-    <path d="M16 16l4-4-4-4" />
+    <path d="M9 3h12" />
+    <path d="M15 3v12" />
+    <path d="M9 15h6" />
+    <path d="M4 21V9a2 2 0 0 1 2-2h3" />
   </svg>
 );
