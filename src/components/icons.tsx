ec1a6380098fbdaa092bdb0ce563876549769c5b
Yash Jha <yjha974@gmail.com>
2025-08-07 17:04:04 +0000

    logo is still bad

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -12,15 +12,10 @@ export const TradeFlowLogo = (props: SVGProps<SVGSVGElement>) => (
             fill="hsl(var(--primary))"
             d="M39.6,256h176.8c21.8,0,39.6-17.8,39.6-39.6V39.6C256,17.8,238.2,0,216.4,0H39.6C17.8,0,0,17.8,0,39.6v176.8 C0,238.2,17.8,256,39.6,256z"
         />
-        <g transform="translate(50 50) scale(1)">
-            <path
-                d="M59,165l46-48 52,32 58-64"
-                fill="none"
-                stroke="hsl(var(--primary-foreground))"
-                strokeWidth="12"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-            />
+        <g fill="hsl(var(--primary-foreground))" transform="translate(55, 50) scale(0.9)">
+            <rect x="20" y="100" width="30" height="60" rx="4" />
+            <rect x="65" y="60" width="30" height="100" rx="4" />
+            <rect x="110" y="20" width="30" height="140" rx="4" />
         </g>
     </svg>
 );
